7230b78a0a81b2a17f85ef4fd0bfe749bb94f5de
Christophe Noël <christophe@taotesting.com>
2016-06-17 10:25:48 +0200

    add scripts for theme registration in a tao extension

---
+++ tao-extension/platform-theme-registration/scripts/install/SetPlatformTheme.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+namespace oat\unisaTheme\scripts\install;
+
+use oat\unisaTheme\model\theme\PlatformDefault;
+use oat\oatbox\service\ServiceManager;
+use oat\tao\model\theme\ThemeService;
+
+class SetPlatformTheme extends \common_ext_action_InstallAction
+{
+    public function __invoke($params)
+    {
+        $serviceManager = ServiceManager::getServiceManager();
+        $themeService = $serviceManager->get(ThemeService::SERVICE_ID);
+        
+        $themeService->setTheme(new PlatformDefault());
+        $serviceManager->register(ThemeService::SERVICE_ID, $themeService);
+
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Plaform theme registered');
+    }
+}
+
---
+++ tao-extension/platform-theme-registration/views/templates/themes/platform/default/header-logo.tpl
@@ -0,0 +1,3 @@
+<a href="" title="" class="lft" target="_blank">
+    <img src="<?=oat\tao\helpers\Template::img('themes/platform/default/logo.png', 'unisaTheme')?>" alt="Logo" id="tao-main-logo">
+</a>
